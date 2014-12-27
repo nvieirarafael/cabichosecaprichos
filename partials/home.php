2d0cc9e8bb4227d3e68b1025cd1f914bfa3dad33
Rafael <nvieirarafael@gmail.com>
2014-12-27 19:43:21 -0200

    testando algumas imagens para o banner

--- partials/home.php
+++ partials/home.php
@@ -3,11 +3,24 @@
     <ol class="carousel-indicators">
         <li data-target="#home_carousel" data-slide-to="0" class="active"></li>
         <li data-target="#home_carousel" data-slide-to="1"></li>
+        <li data-target="#home_carousel" data-slide-to="2"></li>
+        <li data-target="#home_carousel" data-slide-to="3"></li>
+        <li data-target="#home_carousel" data-slide-to="4"></li>
     </ol>
 
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
         <div class="item active">
+            <img src="images/carousel/banner.jpg" alt="" />
+        </div>
+        <div class="item">
+            <img src="images/carousel/banner02.jpg" alt="" />
+        </div>
+        <div class="item">
+            <img src="images/carousel/banner03.jpg" alt="" />
+        </div>
+
+        <div class="item">
             <img src="images/carousel/pitoco.png" alt="" />
             <div class="carousel-caption">
                 <h2>Lorem ipsum delor sit amet</h2>
