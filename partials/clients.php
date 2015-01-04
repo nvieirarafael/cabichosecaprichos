0aae7579ea771f5e5997bea5ec11ec12a72d82bf
Rafael <nvieirarafael@gmail.com>
2015-01-04 15:59:30 -0200

    ajustei fotos dos clientes

--- partials/clients.php
+++ partials/clients.php
@@ -3,32 +3,32 @@
         <div id="carousel">
             <div>
                 <a href="images/dog-1.png" title="Dog" data-hover="Sandy the west highland terrier" data-toggle="lightbox" class="lightbox">
-                    <img src="images/dog-1.png" alt="Dog" />
+                    <img src="images/dogs/1.png" alt="Dog" />
                 </a>
             </div>
             <div>
-                <a href="images/dog-2.png" title="Dog" data-hover="Marty the yorkshire terrier" data-toggle="lightbox" class="lightbox">
-                    <img src="images/dog-2.png" alt="Dog" />
+                <a href="images/dogs/bigger/2.png" title="Dog" data-hover="Marty the yorkshire terrier" data-toggle="lightbox" class="lightbox">
+                    <img src="images/dogs/2.png" alt="Dog" />
                 </a>
             </div>
             <div>
-                <a href="images/dog-3.png" title="Dog" data-hover="Kyla the bull terrier" data-toggle="lightbox" class="lightbox">
-                    <img src="images/dog-3.png" alt="Dog" />
+                <a href="images/dogs/bigger/3.png" title="Dog" data-hover="Pitoco" data-toggle="lightbox" class="lightbox">
+                    <img src="images/dogs/3.png" alt="Dog" />
                 </a>
             </div>
             <div>
-                <a href="images/dog-1.png" title="Dog" data-hover="Marty the yorkshire terrier" data-toggle="lightbox" class="lightbox">
-                    <img src="images/dog-1.png" alt="Dog" />
+                <a href="images/dogs/bigger/4.png" title="Dog" data-hover="Osama" data-toggle="lightbox" class="lightbox">
+                    <img src="images/dogs/4.png" alt="Dog" />
                 </a>
             </div>
             <div>
-                <a href="images/dog-2.png" title="Dog" data-hover="Sandy the west highland terrier" data-toggle="lightbox" class="lightbox">
-                    <img src="images/dog-2.png" alt="Dog" />
+                <a href="images/dogs/bigger/5.png" title="Jade" data-hover="Jade" data-toggle="lightbox" class="lightbox">
+                    <img src="images/dogs/5.png" alt="Jade" />
                 </a>
             </div>
             <div>
-                <a href="images/dog-3.png" title="Dog" data-hover="Kyla the bull terrier" data-toggle="lightbox" class="lightbox">
-                    <img src="images/dog-3.png" alt="Dog" />
+                <a href="images/dogs/bigger/6.png" title="Pitoco" data-hover="Pitoco" data-toggle="lightbox" class="lightbox">
+                    <img src="images/dogs/6.png" alt="Pitoco" />
                 </a>
             </div>
         </div>
